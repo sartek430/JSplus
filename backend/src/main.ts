b1c06551fd66db1b1fd61d72e73aa43ea6b8db19
Jules Sorensen <julessorens@gmail.com>
2023-10-17 16:39:37 +0200

    allow all cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: false});
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.useGlobalPipes(new ValidationPipe());
+  var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Expose-Headers', '*');
+    next();
+  };
+
+  app.use(allowCrossDomain);
   await app.listen(3000);
 
   console.log(`Application is running on: localhost:3000`);

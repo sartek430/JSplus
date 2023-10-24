8bd788448108fdc1fe31ec4d2a213d163e811d89
VAYR_hugo <77483277+sartek430@users.noreply.github.com>
2023-10-24 17:05:08 +0200

    Update Router.tsx

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -9,7 +9,7 @@ const Router: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route
-        path="/MeteoPlus/"
+        path="/MeteoPlus/accueil"
         element={
           <AuthWrapper>
             <HomePage />

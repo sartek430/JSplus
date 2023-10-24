6bb3e5b7ba3006b2bcb7b7be9f0cbcb488d0bd99
VAYR_hugo <77483277+sartek430@users.noreply.github.com>
2023-10-24 16:56:27 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MeteoPlus",
+  base: "/MeteoPlus/accueil",
   server: {
     port: 5173,
     open: "/MeteoPlus/accueil",

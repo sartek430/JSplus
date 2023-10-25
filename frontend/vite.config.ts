4c5cd5b9702e1b955cc795619482c1573b844dce
sartek430 <macogneur430@gmail.comm>
2023-10-25 09:48:57 +0200

    change base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/MeteoPlus",
   server: {
     // this ensures that the browser opens upon server start
     open: true,
